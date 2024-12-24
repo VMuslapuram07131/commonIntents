5525682a8e5c47dd4c0ce29226a9f6d4578a35c1
VK.muslapuram <vk.muslapuram@outlook.com>
2024-12-24 00:09:03 +0000

    Common INtents

---
+++ app/src/main/java/com/example/commonintent/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.commonintent
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun openBrowser(view: View) {
+        val url = findViewById<TextInputEditText>(R.id.url_input).text.toString()
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://$url"))
+        startActivity(intent)
+    }
+}
